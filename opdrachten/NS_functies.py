7b49aa82d71ded41827b53bd88dfc90c7ddde18a
elifpinar7 <elif.pinar@student.hu.nl>
2025-09-16 17:34:55 +0200

    NS_functies.py

--- opdrachten/NS_functies.py
+++ opdrachten/NS_functies.py
@@ -17,49 +17,36 @@ Voeg commentaar toe om je code toe te lichten.
 
 
 def standaardprijs(afstandKM):
-    """
-    Bepaal de prijs van een treinrit. Iedere treinrit kost 80 cent per kilometer,
-    maar als de rit langer is dan 50 kilometer betaal je een vast bedrag van €15,-
-    plus 60 cent per kilometer.
-
-    Ga bij invoer van negatieve afstanden uit van een afstand van
-    0 kilometer (prijs is dan dus 0 Euro).
-
-    Args:
-        afstandKM (int): De reisafstand in kilometers.
-    Returns:
-        float: De berekende standaardprijs.
-    """
-    return
+   if afstandKM < 0: #negatieve afstand
+    return 0
+elif afstandKM > 50: #langere ritten
+    return 15 (afstandKM * 0.60)
+else: #ritten tot en met 50 KM
+    return afstandKM * 0.80
 
 
 def ritprijs(leeftijd, weekendrit, afstandKM):
-    """
-    Het eerste wat deze functie moet doen, is het aanroepen van
-    functie standaardprijs, waarbij de afstand in kilometers doorgegeven
-    moet worden om de standaardprijs voor de rit op te vragen.
+    prijs = standaardprijs(afstandKM) #basisprijs opvragen
 
-    Aan de hand van de standaardprijs kan de actuele ritprijs worden berekend.
-    De regels zijn als volgt:
+    if leeftijd < 12 or leeftijd < 65: #kinderen en ouderen
+        if weekendrit: #weekendprijs kinderen en ouderen
+            prijs * 0.65
+        else: #werkdagprijs
+            prijs * 0.70
+    else: #overige leeftijden
+        if weekend: #weekendprijs overige leeftijden
+            prijs * 0.60
 
-     * Op werkdagen reizen kinderen (onder 12 jaar) en ouderen (65 en ouder) met 30% korting.
-     * In het weekend reist deze groep met 35% korting.
-     * Overige leeftijdsgroepen betalen de gewone prijs, behalve in het weekend. Dan reist
-       deze leeftijdsgroep met 40% korting.
-
-    Args:
-        leeftijd (int): De leeftijd van de reiziger in gehele jaren.
-        weekendrit (bool): True als het een rit in het weekend betreft, anders False.
-        afstandKM (int): De reisafstand in kilometers.
-    Returns:
-        float: De berekende ritprijs.
-    """
-    return
+    return prijs
+    
+   
 
 
 def development_code():
     # Plaats hieronder code om je functies tussentijds te testen. Bijv:
     print("development printout:", standaardprijs(30))
+    print("standaardprijs(80):", standaardprijs(80))
+    
 
 
 def module_runner():
